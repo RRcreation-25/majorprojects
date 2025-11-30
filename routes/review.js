858829aa752c9d934a44f6c7af141e5d96c8500a
RRcreation-25 <raviranjanverma2509@gmail.com>
2025-11-30 13:48:50 +0530

    edit file
    edit

--- routes/review.js
+++ routes/review.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router = express.Router({mergeParams:true})
 const wrapAsync = require("../utils/wrapAsync.js")
-const Listing = require("../majorprojects/models/listing.js");
-const Review = require("../majorprojects/models/review.js");
+const Listing = require("../models/listing.js");
+const Review = require("..models/review.js");
 
 const {validateReview,isLoggedIn,isReviewAuthor} = require("../midelware.js")
 
-const reviewController = require("../majorprojects/controllers/review.js")
+const reviewController = require("../controllers/review.js")
 
 //review
 //post route
