2e976ca9d73b563f3ad546785e5b6efcd32632f5
RRcreation-25 <raviranjanverma2509@gmail.com>
2025-11-30 13:49:23 +0530

    edit
    edit file

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const User = require("../majorprojects/models/user.js")
+const User = require("../models/user.js")
 const wrapAsync = require("../utils/wrapAsync.js")
 const passport = require("passport")
 const {saveRedirectUrl} = require("../midelware.js")
 
-const userController = require("../majorprojects/controllers/users.js")
+const userController = require("../controllers/users.js")
 
 router.route("/signup")
 .get(userController.renderSignUpForm)
